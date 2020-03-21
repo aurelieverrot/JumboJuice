03cc86057b90b2884893ff8e9f3e784d0f21e41a
ancaagp <anca.agapi@gmail.com>
2020-03-20 23:24:58 -0700

    Refactored juice model + added view all juices, view single juice, edit and delete juice

--- fruitcollector/main_app/views.py
+++ fruitcollector/main_app/views.py
@@ -13,53 +13,17 @@ from .forms import JuiceForm, FruitForm
 
 # Create your views here.
 
+# HOMEPAGE
+
 def home(request):
     return render(request, 'home.html')
+
+# ABOUT
 
 def about(request):
     return render(request, 'about.html')
 
-@login_required
-def juices_index(request):
-    juices = Juice.objects.all()
-    return render(request, 'juices/juices_index.html', { 'juices': juices})
-
-@login_required
-def fruits_detail(request, fruit_id):
-    fruit = Fruit.objects.get(id=fruit_id)
-    # gets the vitamins not associated with the fruit
-    vitamins_nonrelated = Vitamin.objects.exclude(id__in = fruit.vitamins.all().values_list('id'))
-    smoothie_form = SmoothieForm()
-    return render(request, 'fruits/detail.html', {'fruit':fruit, 'smoothie_form': smoothie_form, 'vitamins': vitamins_nonrelated })
-
-@login_required
-def add_smoothie(request, fruit_id):
-    form = SmoothieForm(request.POST)
-    # validate the form
-    if form.is_valid():
-        new_smoothie = form.save(commit=False)
-        new_smoothie.fruit_id = fruit_id
-        new_smoothie.save()
-    return redirect('detail', fruit_id=fruit_id)
-
-@login_required
-def assoc_vitamin(request, fruit_id, vitamin_id):
-    Fruit.objects.get(id=fruit_id).vitamins.add(vitamin_id)
-    return redirect('detail', fruit_id=fruit_id)
-
-@login_required
-def new_juice(request):
-    if request.method == 'POST':
-        form = JuiceForm(request.POST)
-        if form.is_valid():
-            juice = form.save(commit=False)
-            juice.user = request.user
-            juice.save()
-            return redirect('detail', juice.id)
-    else:
-        form = JuiceForm()
-    context = {'form': form}
-    return render(request, 'juices/juice_form.html', context)
+# SIGNUP
 
 def signup(request):
     error_message=''
@@ -76,6 +40,96 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+# VIEW ALL JUICES
+
+@login_required
+def juices_index(request):
+    juices = Juice.objects.all()
+    return render(request, 'juices/index.html', { 'juices': juices} )
+
+# CREATE NEW JUICE
+
+@login_required
+def new_juice(request):
+    if request.method == 'POST':
+        form = JuiceForm(request.POST)
+        if form.is_valid():
+            juice = form.save(commit=False)
+            juice.user = request.user
+            juice.save()
+            return redirect('juice_detail', juice.id)
+    else:
+        form = JuiceForm()
+    context = {'form': form}
+    return render(request, 'juices/juice_form.html', context)
+
+# VIEW JUICE DETAIL
+
+@login_required
+def juice_detail(request, juice_id):
+    juice = Juice.objects.get(id=juice_id)
+    # gets the vitamins not associated with the fruit
+    # vitamins_nonrelated = Vitamin.objects.exclude(id__in = fruit.vitamins.all().values_list('id'))
+    juice_form = JuiceForm()
+    return render(request, 'juices/detail.html', {'juice':juice, 'juice_form': juice_form })
+
+# JUICE UPDATE
+
+def juice_update(request, juice_id):
+    juice = Juice.objects.get(id=juice_id)
+    if request.method == 'POST':
+        form = JuiceForm(request.POST, instance=juice)
+        juice = form.save()
+        return redirect('juice_detail', juice.id)
+    else:
+        form = JuiceForm(instance=juice)
+    return render(request, 'juices/juice_form.html', { 'form': form})
+
+# JUICE DELETE
+
+def juice_delete(request, juice_id):
+    Juice.objects.get(id=juice_id).delete()
+    return redirect('juices_index')
+
+
+#------------------------------ OTHER
+
+
+@login_required
+def fruits_index(request):
+    fruits = Fruit.objects.all()
+    return render(request, 'fruits/index.html', { 'fruits': fruits })
+
+@login_required
+def fruits_detail(request, fruit_id):
+    fruit = Fruit.objects.get(id=fruit_id)
+    # gets the vitamins not associated with the fruit
+    vitamins_nonrelated = Vitamin.objects.exclude(id__in = fruit.vitamins.all().values_list('id'))
+    smoothie_form = SmoothieForm()
+    return render(request, 'fruits/detail.html', {'fruit':fruit, 'smoothie_form': smoothie_form, 'vitamins': vitamins_nonrelated })
+
+
+@login_required
+def assoc_vitamin(request, fruit_id, vitamin_id):
+    Fruit.objects.get(id=fruit_id).vitamins.add(vitamin_id)
+    return redirect('detail', fruit_id=fruit_id)
+
+@login_required
+def new_fruit(request):
+    if request.method == 'POST':
+        form = FruitForm(request.POST)
+        if form.is_valid():
+            fruit = form.save(commit=False)
+            fruit.user = request.user
+            fruit.save()
+            return redirect('detail', fruit.id)
+    else:
+        form = FruitForm()
+    context = {'form': form}
+    return render(request, 'fruits/fruit_form.html', context)
+
+
 
 def fruits_update(request, fruit_id):
     fruit = Fruit.objects.get(id=fruit_id)
